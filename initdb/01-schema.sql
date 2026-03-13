b6b5596ec2da4190cecccfc2029d33d3b5d96268
JayleenCurty <jayleen.curty@hes-so.ch>
2026-03-13 12:00:38 +0100

    creation tables yeahhh

--- initdb/01-schema.sql
+++ initdb/01-schema.sql
@@ -1,17 +1,105 @@
--- =========================================
--- SCHEMA: movies_db
--- =========================================
+-- SCHEMA:
 
----CREATE TABLE movies (
-   --- id SERIAL PRIMARY KEY,
-   --- title VARCHAR(100) NOT NULL,
-    ---release_year INT CHECK (release_year >= 1888 AND release_year <= 2025)
----);
+-- table libelle de type intervention, liée à la table INTERVENTION
+CREATE TABLE type_intervention (
+    id SERIAL PRIMARY KEY,
+    type_intervention VARCHAR(100) UNIQUE NOT NULL
+);
 
----CREATE TABLE boxoffice (
-    --id SERIAL PRIMARY KEY,
-    ---movie_id INT NOT NULL REFERENCES movies(id),
-   -- rating DECIMAL(3,1) CHECK (rating >= 0 AND rating <= 10),
-   -- domestic_sales INT CHECK (domestic_sales >= 0),
-    --international_sales INT CHECK (international_sales >= 0)
---)
+-- Interventions
+CREATE TABLE intervention (
+    id SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    objet VARCHAR(255),
+    technicien VARCHAR(255),
+    duree INTERVAL, -- ou DECIMAL pour heures
+    cout_materiel NUMERIC(10, 2),
+    remarque TEXT,
+    id_type_intervention INTEGER NOT NULL REFERENCES type_intervention (id)
+);
+
+-- table libelle de statut signalement, liée à la table SIGNALEMENT
+CREATE TABLE statut_signalement (
+    id SERIAL PRIMARY KEY,
+    statut VARCHAR(100) NOT NULL
+);
+
+-- table libelle de statut urgence, liée à la table SIGNALEMENT
+CREATE TABLE statut_urgence (
+    id SERIAL PRIMARY KEY,
+    statut VARCHAR(100) NOT NULL
+);
+
+-- table libelle de lieux inventaire, liée à la table INVENTAIRE
+CREATE TABLE lieux_inventaire (
+    id SERIAL PRIMARY KEY,
+    lieux VARCHAR(255) NOT NULL,
+    latitude DECIMAL(10, 8) NOT NULL,
+    longitude DECIMAL(11, 8) NOT NULL 
+);
+
+-- table libelle de etat_iventaire, liée à la table INVENTAIRE
+CREATE TABLE etat_inventaire (
+    id SERIAL PRIMARY KEY,
+    etat VARCHAR(100) NOT NULL
+);
+
+-- table libelle de type_iventaire, liée à la table INVENTAIRE
+CREATE TABLE type_inventaire (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(100) NOT NULL
+);
+
+-- table libelle de type_materiaux, liée à la table INVENTAIRE
+CREATE TABLE type_materiaux (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(100) NOT NULL
+);
+
+-- table libelle de type_materiels, liée à la table FOURNISSEURS
+CREATE TABLE type_materiels (
+    id SERIAL PRIMARY KEY,
+    type_materiels VARCHAR(100) NOT NULL
+);
+
+-- Fournisseurs
+CREATE TABLE fournisseurs (
+    id SERIAL PRIMARY KEY,
+    entreprises VARCHAR(255),
+    contact VARCHAR(255),
+    telephone VARCHAR(20) UNIQUE,
+    email VARCHAR(255) UNIQUE,
+    remarque TEXT,
+    id_type_materiel INTEGER NOT NULL REFERENCES type_materiaux(id)
+
+-- Inventaire (table centrale)
+CREATE TABLE inventaire (
+    id SERIAL PRIMARY KEY,
+    id VARCHAR(100) UNIQUE NOT NULL, -- "ID" comme identifiant unique
+    date_installation DATE,
+    remarque TEXT,
+    id_fournisseurs INTEGER NOT NULL REFERENCES fournisseurs (id),
+    id_type_inventaire INTEGER NOT NULL REFERENCES type_inventaire (id),
+    id_type_materiaux INTEGER NOT NULL REFERENCES type_materiaux (id),
+    id_etat_inventaire INTEGER NOT NULL REFERENCES etat_inventaire (id),
+    id_lieux_inventaire INTEGER NOT NULL REFERENCES lieux_inventaire (id)
+);
+
+-- Signalement
+CREATE TABLE signalement (
+    id SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    signal_par VARCHAR(255),
+    objet VARCHAR(255),
+    description TEXT,
+    id_statut_signalement INTEGER NOT NULL REFERENCES statut_signalement (id),
+    id_statut_urgence INTEGER NOT NULL REFERENCES statut_urgence (id),
+    id_inventaire INTEGER NOT NULL REFERENCES inventaire (id)
+);
+
+-- Table de liaison Signalement-Intervention (N-N)
+CREATE TABLE signalement_intervention (
+    id SERIAL PRIMARY KEY,
+    id_signalement INTEGER NOT NULL REFERENCES signalement(id),
+    id_intervention INTEGER NOT NULL REFERENCES intervention(id)
+);
