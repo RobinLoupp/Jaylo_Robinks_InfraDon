4f59866684584140f0ba694e37dd64ea1a979595
Robin Loup <robin.loup@heig-vd.ch>
2026-03-20 11:07:47 +0100

    staging

---
+++ initdb/02-staging.sql
@@ -0,0 +1,62 @@
+-- Active: 1772187879165@@127.0.0.1@5432@postgres
+-- Créer un schéma dédié pour la staging
+CREATE SCHEMA IF NOT EXISTS staging;
+
+CREATE TABLE staging.fournisseurs_contacts (
+    entreprise TEXT,
+    contact TEXT,
+    telephone TEXT,
+    email TEXT,
+    type_materiel TEXT,
+    remarque TEXT
+);
+
+
+CREATE TABLE staging.interventions (
+  
+  date              TEXT,
+	objet             TEXT,
+  type_intervention TEXT,
+  technicien        TEXT,
+  duree             TEXT,
+  cout_materiel     TEXT,
+  remarque          TEXT
+);
+
+
+CREATE TABLE staging.inventaire_mobilier (
+id TEXT,
+	type              TEXT,
+  materiau          TEXT,
+  lieu              TEXT,
+  latitude          TEXT,
+  longitude         TEXT,
+  date_installation TEXT,
+  etat              TEXT,
+  remarques         TEXT
+);
+
+CREATE TABLE staging.signalements (
+  date          TEXT,
+  signale_par   TEXT,
+  objet         TEXT,
+  description   TEXT,
+  urgence       TEXT,
+  statut        TEXT
+);
+
+COPY staging.fournisseurs_contacts
+FROM '/data/fournisseurs_contacts.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8');
+
+COPY staging.interventions
+FROM '/data/interventions.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8');
+
+COPY staging.inventaire_mobilier
+FROM '/data/inventaire_mobilier.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8');
+ 
+COPY staging.signalements
+FROM '/data/signalements.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8');
